9a870f5439871a79ef8fe7da656d7e13611a0dcf
LAPTOP-6EK9OKR7\Owner <brie.noellee@gmail.com>
2026-01-14 12:59:53 -0500

    Coding Challenge 2 completed

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -6,11 +6,14 @@ public class Timer : MonoBehaviour
     public float newSpotX;
     public float newSpotY;
 
+    public float teleport = 0; 
+    public float timer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        newSpotX = Random.Range(-10f, 10f);
-        newSpotY = Random.Range(-5f, 5f);
+
+        timer = Time.deltaTime;
 
         transform.position = (Vector2)transform.position + Random.insideUnitCircle * 2;
     }
@@ -18,11 +21,20 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = transform.position;
-        newPosition.x = newSpotX;
-        newPosition.y = newSpotY;
-        transform.position = newPosition;
+        teleport += timer;
+        if (teleport > 3)
+        {
+            newSpotX = Random.Range(-10f, 10f);
+            newSpotY = Random.Range(-5f, 5f);
 
-       
+            Vector3 newPosition = transform.position;
+            newPosition.x = newSpotX;
+            newPosition.y = newSpotY;
+            transform.position = newPosition;
+            teleport = 0;
+        }
+
+
+
     }
 }
