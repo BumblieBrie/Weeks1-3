972794e265af8204a9d8dfcab6dbfdbbff9f74c3
LAPTOP-6EK9OKR7\Owner <brie.noellee@gmail.com>
2026-01-21 12:31:46 -0500

    CLOCK completed

--- Assets/Script/Ticking.cs
+++ Assets/Script/Ticking.cs
@@ -6,7 +6,6 @@ public class Ticking : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        speed = 0f;
         
     }
 
