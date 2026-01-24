ab3baeb7846a7d73b2193cee60d60b57b7043836
LAPTOP-6EK9OKR7\Owner <brie.noellee@gmail.com>
2026-01-24 00:37:38 -0500

    Ooops, previous commit was supposed to go here Made the raindrop fall properly, need to fix the screen height

--- Assets/Script/Assignment 1/RaindropMovement.cs
+++ Assets/Script/Assignment 1/RaindropMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class RaindropMovement : MonoBehaviour
 {
-    public float speed = 0.5f;
+    public float speed = 0.05f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,19 +18,20 @@ public class RaindropMovement : MonoBehaviour
         //make the raindrop reappear at the top of the screen when it goes off the bottom
         //fix screen position to world position for the screen height
         //uhhh fix the raindrop from falling eternally
+        //make it so that it appears at the top of the screen no matter what size the screen is
 ;
 
         Vector2 newPosition = transform.position;
         newPosition.y -= speed;
-        transform.position = newPosition;
 
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (newPosition.y < -5)
+        if (screenPos.y < -Screen.height)
         {
-            newPosition.y = 0;
+            newPosition.y = 5f;
 
         }
+        transform.position = newPosition;
 
         Debug.Log(screenPos);
 
